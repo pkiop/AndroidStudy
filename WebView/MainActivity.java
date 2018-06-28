5e3015e0d1d6ce0492cd3478765356ea52aa9742
pkiop <vivezdv@gmail.com>
2018-06-28 16:21:03 +0900

    webview

---
+++ WebView/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.pkiop.myweb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.loadUrl("file:///android_asset/sample.html"); //assets 라고 하면 안된다.
+
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.loadUrl("http://www.naver.com");
+            }
+        });
+    }
+}
---
+++ WebView/readme.md
@@ -0,0 +1,19 @@
+위젯 -> 웹뷰
+
+=> 앱 안에 웹 브라우저가 들어갔다고 생각하면 된다 .
+
+하다가 
+
+1.프로젝트에서 app 오른쪽 클릭 -> new -> forder -> assert folder
+
+인터넷 사용하게 하고 싶으면 
+
+<uses-permission android:name="android.permission.INTERNET"/>
+
+을 메니페스트파일에 추가 
+
+
+edittext에서 어떤 키보드가 나올 지 선택가능
+
+inputtype에서 선택
+<img src="./pic/a.jpg" width="200">
